f71bb92ca9c3e866a0069c8a214d6951cccb2bbd
asreimer <ashtonsethreimer@gmail.com>
2020-05-03 16:48:48 -0700

    removing autolink default role

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,6 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
-
-default_role = "autolink"
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
